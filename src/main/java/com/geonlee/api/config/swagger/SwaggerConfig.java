02eac2678b3652b0945104bdccef39943cdf1217
GEONLEE <geonlee@neighbor21.co.kr>
2024-08-13 16:01:29 +0900

    [feature] swagger operation customizing

--- src/main/java/com/geonlee/api/config/swagger/SwaggerConfig.java
+++ src/main/java/com/geonlee/api/config/swagger/SwaggerConfig.java
@@ -1,13 +1,20 @@
 package com.geonlee.api.config.swagger;
 
+import com.geonlee.api.common.code.ErrorCode;
+import com.geonlee.api.common.response.ErrorResponse;
+import com.geonlee.api.config.message.MessageConfig;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
 import org.springdoc.core.customizers.OperationCustomizer;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -35,20 +42,61 @@ import org.springframework.context.annotation.Configuration;
         externalDocs = @ExternalDocumentation(description = "Swagger reference", url = "https://springdoc.org/properties.html")
 )
 @Configuration
+@RequiredArgsConstructor
 public class SwaggerConfig {
-        @Bean
-        public GroupedOpenApi version1APi() {
-                return GroupedOpenApi.builder()
-                        .group("v1.0")
-                        .pathsToMatch("/v1/**")
-                        .build();
-        }
 
-        @Bean
-        public GroupedOpenApi version2APi() {
-                return GroupedOpenApi.builder()
-                        .group("v2.0")
-                        .pathsToMatch("/v2/**")
-                        .build();
-        }
+    private final MessageConfig messageConfig;
+
+    @Bean
+    public GroupedOpenApi version1APi() {
+        return GroupedOpenApi.builder()
+                .group("v1.0")
+                .pathsToMatch("/v1/**")
+                .addOperationCustomizer(operationCustomizer())
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi version2APi() {
+        return GroupedOpenApi.builder()
+                .group("v2.0")
+                .pathsToMatch("/v2/**")
+                .build();
+    }
+
+    @Bean
+    public OperationCustomizer operationCustomizer() {
+        return (operation, handlerMethod) -> {
+            ApiResponses apiResponses = operation.getResponses();
+            if (apiResponses.containsKey("200")) {
+                ApiResponse normalResponse = operation.getResponses().get("200");
+                normalResponse.description("정상 응답");
+                apiResponses.put("OK", normalResponse);
+                apiResponses.remove("200");
+            }
+            apiResponses.addApiResponse("ERR_CE_01", getInvalidParameterResponse());
+            return operation;
+        };
+    }
+
+    public ApiResponse getInvalidParameterResponse() {
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setDescription("유효하지 않은 인자가 전달됨");
+        addContent(apiResponse, ErrorCode.INVALID_PARAMETER);
+        return apiResponse;
+    }
+
+    //    @SuppressWarnings("rawtypes")
+    private void addContent(ApiResponse apiResponse, ErrorCode errorCode) {
+        Content content = new Content();
+        MediaType mediaType = new MediaType();
+        Schema<Object> schema = new Schema<>();
+        schema.$ref("#/components/schemas/ErrorResponse");
+        mediaType.schema(schema).example(ErrorResponse.builder()
+                .status(messageConfig.getCode(errorCode))
+                .message(messageConfig.getMessage(errorCode))
+                .build());
+        content.addMediaType("application/json", mediaType);
+        apiResponse.setContent(content);
+    }
 }
