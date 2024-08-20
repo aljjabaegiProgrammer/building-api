6f024a5969108965f46eb7d491854d856c63a196
GEONLEE <geonlee@neighbor21.co.kr>
2024-08-20 15:48:58 +0900

    [feature]mapping 설정 추가

--- src/main/java/com/geonlee/api/domin/authority/AuthorityMapper.java
+++ src/main/java/com/geonlee/api/domin/authority/AuthorityMapper.java
@@ -18,4 +18,17 @@ import org.mapstruct.Named;
  */
 @Mapper(componentModel = "spring", imports = Converter.class)
 public interface AuthorityMapper extends GenericMapper<AuthoritySearchResponse, Authority> {
+
+    @Mappings({
+            @Mapping(target = "authorityName", source = "authority.authorityName"),
+            @Mapping(target = "createDate", expression = "java(Converter.localDateTimeToFormattedString(entity.getCreateDate()))"),
+            @Mapping(target = "updateDate", expression = "java(Converter.localDateTimeToFormattedString(entity.getUpdateDate()))")
+    })
+    MemberSearchResponse toMemberRecord(Member entity);
+
+    @Mappings({
+            @Mapping(target = "createDate", expression = "java(Converter.localDateTimeToFormattedString(entity.getCreateDate()))"),
+            @Mapping(target = "updateDate", expression = "java(Converter.localDateTimeToFormattedString(entity.getUpdateDate()))")
+    })
+    AuthoritySearchResponse toRecord(Authority entity);
 }
